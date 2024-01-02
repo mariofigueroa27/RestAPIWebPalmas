c34523975518038d0ada7d8575be37434c4cd886
mariofigueroa27 <mariofigueroa209@outlook.com>
2024-01-02 10:18:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sql = require('mssql');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(cors());
 
